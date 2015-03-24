3983d754539b0fc38f86ac3e2af53fda6cbd6390
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-03-24 10:02:17 +0700

    First Commit

---
+++ Cookie/Cookie.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Cookie - Library untuk framework kecik, library ini khusus untuk membantu masalah Cookie 
+ *
+ * @author 		Dony Wahyu Isp
+ * @copyright 	2015 Dony Wahyu Isp
+ * @link 		http://github.io/kecik_cookie
+ * @license		MIT
+ * @version 	1.0.1-alpha
+ * @package		Kecik\Cookie
+ **/
+namespace Kecik;
+
+/**
+ * Controller
+ * @package 	Kecik\Cokie
+ * @author 		Dony Wahyu Isp
+ * @since 		1.0.1-alpha
+ **/
+class Cookie {
+	/**
+	 * @var bool $status
+	 **/
+	private $status=FALSE;
+
+	private $key;
+	private $iv;
+
+
+	/**
+ 	 * __construct
+ 	 * @param Kecik $app
+ 	 **/
+	public function __construct(Kecik $app) {
+		$config = $app->config;
+		if ( empty( $this->status = $config->get('cookie.encrypt') ) )
+			$this->status = FALSE;
+		else { 
+			if ( empty( $key = $config->get('cookie.encrypt.key') ) )
+				$this->key = pack('H*', $key);
+			else
+				$this->key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+
+			$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+            $this->iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+		}
+
+	}
+
+	/**
+	 * set
+	 * @param string $name
+	 * @param mixed $value
+	 **/
+	public function set($name, $value, $expire=0, $path='', $domain='', $secure=FALSE, $httponly=FALSE) {
+		return setcookie($name, $this->encrypt( $value ), $expire, $path, $domain, $secure, $httponly);
+	}
+
+	/**
+	 * get
+	 * @param string $name
+	 * @return mixed
+	 **/
+	public function get($name) {
+		if (isset($_COOKIE[$name]))
+			return $this->decrypt( $_COOKIE[$name] );
+		else
+			return NULL;
+	}
+
+	/**
+	 * delete
+	 * @param string $name
+	 **/
+	public function delete($name) {
+		if (isset($_COOKIE[$name]))
+			unset($_COOKIE[$name]);
+	}
+
+	/**
+	 * clear
+	 * Hapus semua session
+	 **/
+	public function clear() {
+		while (list($name, $value) = each($_COOKIE)) {
+			$this->delete($name);
+		}
+	}
+
+	/**
+	 * encrypt
+	 * enkripsi cookie
+	 * @param string $plaintext
+	 * @return string
+	 **/
+	private function encrypt($plaintext) {
+		if (is_array($plaintext))
+			$ciphertext = json_encode($plaintext);
+		else
+			$ciphertext = $plaintext;
+
+		if ($this->status) {
+			$ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $this->key, $ciphertext, MCRYPT_MODE_CBC, $this->iv);
+		}
+
+		return $ciphertext;
+	}
+
+	/**
+	 * decrypt
+	 * dekripsi cookie
+	 * @param string $ciphertext
+	 * @return mixed
+	 **/
+	private function decrypt($ciphertext) {
+		$plaintext = $ciphertext;
+		if ($this->status) {
+			$plaintext = trim( mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $this->key, $ciphertext, MCRYPT_MODE_CBC, $this->iv) );
+			$plaintext_array = json_decode($plaintext, TRUE);
+			if ($plaintext_array)
+				$plaintext = $plaintext_array;
+		}
+
+		return $plaintext;
+	}
+}
