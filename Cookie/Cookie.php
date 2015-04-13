76b9cdcf6c98ae54f882a1cb1dc35d6d23fbae2e
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-13 18:30:12 +0700

    fix encryption bug

--- Cookie/Cookie.php
+++ Cookie/Cookie.php
@@ -56,6 +56,9 @@ class Cookie {
 
 			if (empty($_COOKIE['eivk'.md5($app->url->baseUrl())]) || !ctype_print($_COOKIE['eivk'.md5($app->url->baseUrl())]))
 				unset($_SESSION['eivk'.md5($app->url->baseUrl())]);
+			
+			if (empty($this->get('eivk'.md5($app->url->baseUrl()))) || !ctype_print($this->get('eivk'.md5($app->url->baseUrl()))))
+				unset($_SESSION['eivk'.md5($app->url->baseUrl())]);
 
 			if (empty($_COOKIE['eivk'.md5($app->url->baseUrl())]) && empty($_SESSION['eivk'.md5($app->url->baseUrl())])) {
 				$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
@@ -74,6 +77,16 @@ class Cookie {
         	}
 		}
 
+	}
+
+	/**
+	 * newIV
+	 **/
+	public function newIV() {
+		$this->iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+        $this->iv = base64_encode($this->iv);
+        setcookie('eivk'.md5($app->url->baseUrl()), $this->iv, 0, '/');
+        $_SESSION['eivk'.md5($app->url->baseUrl())] = $this->iv;
 	}
 
 	/**
